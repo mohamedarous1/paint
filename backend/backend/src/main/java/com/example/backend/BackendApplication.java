c3208a80bd53327197a23599daae287e0bab7286
mohamedarous1 <mohamed.arous940@gmail.com>
2022-12-06 22:24:36 +0200

    second commit

--- backend/backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.paint.paint;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaintApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaintApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.paint.paint;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaintApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
