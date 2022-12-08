cb6d94c3172fedc75b84dbfcd5b8092b5c3787ce
mohamedarous1 <mohamed.arous940@gmail.com>
2022-12-08 13:49:35 +0200

    UI temporary done

--- frontend/paint_frontend/src/app/app.module.ts
+++ frontend/paint_frontend/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ScreenComponent } from './home/screen/screen.component';
+import { ToolsComponent } from './home/tools/tools.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ScreenComponent,
+    ToolsComponent,
   ],
   imports: [
     BrowserModule
