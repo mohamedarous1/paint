179ce4aff9153f59f9f571b39d23802d20d32950
Michael <michaael.monir119@gmail.com>
2022-12-11 21:12:14 +0200

    "After handling Create Request"

--- frontend/paint_frontend/src/app/home/home.component.ts
+++ frontend/paint_frontend/src/app/home/home.component.ts
@@ -5,7 +5,6 @@ import { Konva } from "konva/cmj/_FullInternals";
 import {factoryShape} from "./factoryShape";
 import { HitCanvas } from 'konva/cmj/Canvas';
 import { Conditional } from '@angular/compiler';
-import {request} from './request'
 import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs";
 import {HttpService} from "../http.service";
@@ -48,6 +47,14 @@ export class HomeComponent implements OnInit {
     this.stage.on('click',  (e:any) => this.OnClickOnStageSelect(e));
   }
 
+
+  SelectShape(SelectedShape:any)
+  {
+    this.selected = SelectedShape;
+    this.selected.draggable(true);
+    this.tr.nodes([this.selected]);
+  }
+
   SelectButtonClick()
   {
     this.ClearEventListeners();
@@ -69,8 +76,8 @@ export class HomeComponent implements OnInit {
     // This part is for requesting from backend
     this.selected.on('transformend', (e:any) =>
     {
+      this.RepairDimentions();
       this.http.edit_pos_sizeRequest(this.selected).subscribe(e=>{});
-      console.log(this.hashmap[id]);
       console.log(this.GetNewSizeAndPosition());
     });
 
@@ -79,12 +86,70 @@ export class HomeComponent implements OnInit {
       console.log(this.GetNewSizeAndPosition());
     });
   }
-  SelectShape(SelectedShape:any)
+
+
+
+
+  BrushClick()
   {
-    this.selected = SelectedShape;
-    this.selected.draggable(true);
-    this.tr.nodes([this.selected]);
+    this.ClearEventListeners();
+    //this.stage.on('click', (e:any)=>this.OnClickOnStageBrush(e));
+    this.OnClickOnStageBrush();
   }
+  OnClickOnStageBrush()
+  {
+    let brushcolor = document.getElementById("favcolor");
+    brushcolor?.addEventListener('input', (e:any)=>this.BrushColorValue(e));
+    var isPaint = false;
+    var mode = 'brush';
+    var lastLine : any;
+
+    this.stage.on('mousedown touchstart',  (e:any) =>
+    {
+      isPaint = true;
+      var pos = this.stage.getPointerPosition();
+      lastLine = new Konva.Line({
+        id:'50',
+        stroke: this.BruchColor,
+        strokeWidth: 5,
+        globalCompositeOperation:
+          mode === 'brush' ? 'source-over' : 'destination-out',
+        // round cap for smoother lines
+        lineCap: 'round',
+        lineJoin: 'round',
+        // add point twice, so we have some drawings even on a simple click
+        points: [pos.x, pos.y, pos.x, pos.y],
+      });
+      this.layer.add(lastLine);
+    });
+
+    this.stage.on('mouseup touchend', ()=>
+    {
+
+      isPaint = false;
+    });
+
+
+    // and core function - drawing
+    this.stage.on('mousemove touchmove',  (e:any) => {
+      if (!isPaint)
+      {
+        return;
+      }
+
+      // prevent scrolling on touch devices
+      e.evt.preventDefault();
+
+      const pos = this.stage.getPointerPosition();
+      var newPoints = lastLine.points().concat([pos.x, pos.y]);
+      lastLine.points(newPoints);
+    });
+
+  }
+  BrushColorValue(e:any){
+    this.BruchColor = e.target.value;
+  }
+
 
   GetNewSizeAndPosition()
   {
@@ -115,7 +180,6 @@ export class HomeComponent implements OnInit {
       temp.fill("color");
     }
     this.layer.add(temp);
-    this.id = this.id +1;
     this.selected = temp;
     this.tr.nodes([this.selected])
     this.SelectButtonClick();
@@ -135,60 +199,6 @@ export class HomeComponent implements OnInit {
     Shape.id(id);
     console.log(id);
     this.hashmap[id] = ShapeType;
-  }
-
-  BrushClick()
-  {
-    this.ClearEventListeners();
-    this.stage.on('click', (e:any)=>this.OnClickOnStageBrush(e));
-  }
-  OnClickOnStageBrush(ee:any)
-  {
-    let brushcolor = document.getElementById("favcolor");
-    brushcolor?.addEventListener('input', (e:any)=>this.BrushColorValue(e));
-    var isPaint = false;
-    var mode = 'brush';
-    var lastLine : any;
-
-    this.stage.on('mousedown touchstart',  (e:any) =>
-    {
-      isPaint = true;
-      var pos = this.stage.getPointerPosition();
-      lastLine = new Konva.Line({
-        stroke: this.BruchColor,
-        strokeWidth: 5,
-        globalCompositeOperation:
-          mode === 'brush' ? 'source-over' : 'destination-out',
-        // round cap for smoother lines
-        lineCap: 'round',
-        lineJoin: 'round',
-        // add point twice, so we have some drawings even on a simple click
-        points: [pos.x, pos.y, pos.x, pos.y],
-      });
-      this.layer.add(lastLine);
-    });
-
-    this.stage.on('mouseup touchend', function () {
-      isPaint = false;
-    });
-
-    // and core function - drawing
-    this.stage.on('mousemove touchmove',  (e:any) => {
-      if (!isPaint) {
-        return;
-      }
-
-      // prevent scrolling on touch devices
-      e.evt.preventDefault();
-
-      const pos = this.stage.getPointerPosition();
-      var newPoints = lastLine.points().concat([pos.x, pos.y]);
-      lastLine.points(newPoints);
-    });
-
-  }
-  BrushColorValue(e:any){
-    this.BruchColor = e.target.value;
   }
 
 
@@ -233,6 +243,43 @@ export class HomeComponent implements OnInit {
     this.tr.nodes([]);
     ClickedShape.destroy();
     this.ClearEventListeners();
+  }
+
+  RepairDimentions()
+  {
+    let shape = this.selected;
+    if (shape == undefined) return;
+    let id:number = shape.id();
+    let ShapeType = this.hashmap[id];
+
+
+    if (ShapeType == "Square" || ShapeType == "Rectangle")
+    {
+      this.selected.width( this.selected.width()* this.selected.scaleX());
+      this.selected.height(  this.selected.height()*this.selected.scaleY());
+    }
+    else if (ShapeType == "Ellipse")
+    {
+      this.selected.radiusX( this.selected.width()* this.selected.scaleX());
+      this.selected.radiusY(  this.selected.height()*this.selected.scaleY());
+    }
+    else if (ShapeType == "Circle")
+    {
+      this.selected.radius( this.selected.width()* this.selected.scaleX());
+    }
+    else if (ShapeType == "Triangle")
+    {
+      this.selected.radius( this.selected.radius()* this.selected.scaleX());
+    }
+    else if (ShapeType == "Ellipse")
+    {
+      this.selected.scaleX( this.selected.scaleX()* this.selected.scaleX());
+      this.selected.scaleY( this.selected.scaleY()* this.selected.scaleY());
+    }
+
+
+    this.selected.scaleX(1);
+    this.selected.scaleY(1);
   }
 
 }
